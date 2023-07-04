c8b476776d28a843b5a5bd7df7388d4135ff76c5
NickShelud <N.She.1997@mail.ru>
2023-07-04 16:52:17 +0300

    fix bugs

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -25,15 +25,12 @@
                 </div>
                 <div class="flex lg:order-2">
                 @guest
-                    <button><a href="{{route('login')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{__('trans.nav.login')}}</a></button>
+                    <button name='login'><a href="{{route('login')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{__('trans.nav.login')}}</a></button>
                     <button name='registration'><a href="{{route('register')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">{{__('trans.nav.registration')}}</a></button>
                 @endguest
                 @auth
 
-                <a href="https://php-task-manager-ru.hexlet.app/logout" onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
-                                {{__('trans.nav.logout')}}
-                            </a>
+                <button name='logout'><a href="{{route('login')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{__('trans.nav.logout')}}</a></button>
                     
                 @endauth
                 </div>
