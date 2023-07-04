ed5c44ac0e850a0d3965169437fdb3034a8d2f4b
NickShelud <N.She.1997@mail.ru>
2023-07-04 17:10:55 +0300

    fix bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
             @endif
 
             <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+                {{ __('trans.nav.login') }}
             </x-primary-button>
         </div>
     </form>
