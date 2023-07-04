30326a37b582de581f6955d5fa64fcfeed8bbfd2
NickShelud <N.She.1997@mail.ru>
2023-07-04 22:04:16 +0300

    fix bugs

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -4,7 +4,7 @@ return [
     'required' => 'Это обязательное поле',
     'unique' => ':attribute с таким именем уже существует',
     'min' => [
-        'string' => ':attribute должен иметь длинну не менее :min символов.',
+        'string' => ':attribute должен иметь длину не менее :min символов.',
     ],
     'attributes' => [
         'password' => 'Пароль',
