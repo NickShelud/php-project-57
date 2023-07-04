d4162875225aa5dacf89919abd83e2a572dcc572
NickShelud <N.She.1997@mail.ru>
2023-07-04 17:06:45 +0300

    fix bugs

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -30,8 +30,12 @@
                 @endguest
                 @auth
 
-                <button name='logout'><a href="{{route('login')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{__('trans.nav.logout')}}</a></button>
-                    
+                <a href="{{ route('logout') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Выход</a>
+
+                <!-- Форма для выхода -->
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>    
                 @endauth
                 </div>
             </div>
