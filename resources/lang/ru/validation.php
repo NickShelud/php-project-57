4287704c3a0a8e8f698e37e10bad736eb04df91d
NickShelud <N.She.1997@mail.ru>
2023-07-04 21:58:59 +0300

    fix bugs

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    'namee' => 'тест'
+    'required' => 'Это обязательное поле',
+    'unique' => ':attribute с таким именем уже существует',
+    'min' => [
+        'string' => ':attribute должен иметь длинну не менее :min символов.',
+    ],
+    'attributes' => [
+        'password' => 'Пароль',
+        'status' => 'Статус',
+        'label' => 'Метка',
+    ],
 ];
