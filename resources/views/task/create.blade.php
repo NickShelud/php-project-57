4b0cfb338a291c60ee7b7fce9bea27d7904fe8e4
NickShelud <N.She.1997@mail.ru>
2023-07-05 18:38:43 +0300

    fix bugs

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -7,7 +7,7 @@
     {{ __('trans.task.createTask') }}
 </h1>   
     @include('task.form')
-    {{ Form::submit(__('trans.task.create'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2']) }}
+    {{ Form::submit(__('trans.create'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2']) }}
 </div>
 {{ Form::close() }}
 @endsection
