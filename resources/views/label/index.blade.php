16ead8f1e92c2de39d6b5c399ec93ee2c21ce92a
NickShelud <N.She.1997@mail.ru>
2023-07-05 12:51:50 +0300

    fix bugs click create label

--- resources/views/label/index.blade.php
+++ resources/views/label/index.blade.php
@@ -4,11 +4,11 @@
 
     @auth
     <div>
-        <a href="{{route('label.create')}}" 
+        <a href="{{route('labels.create')}}" 
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" 
                 onclick="event.preventDefault(); document.getElementById('create-form').submit();">{{__('trans.create')}}</a>
 
-            <form id="create-form" action="{{route('label.create')}}" method="GET" style="display: none;">
+            <form id="create-form" action="{{route('labels.create')}}" method="GET" style="display: none;">
                 @csrf
             </form>    
         </div>
