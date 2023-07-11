ec60adaafc33f4f3b58fbc239d94fc12edfea5fc
NickShelud <N.She.1997@mail.ru>
2023-07-11 17:50:55 +0300

    fix bugs in LabelTest

--- tests/Feature/Label/CreateTest.php
+++ tests/Feature/Label/CreateTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Feature;
+
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use App\Http\Controllers\Auth;
--- tests/Feature/Label/IndexTest.php
+++ tests/Feature/Label/IndexTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Feature;
+
 use App\Models\User;
 use App\Models\TaskStatuses;
 use App\Providers\RouteServiceProvider;
