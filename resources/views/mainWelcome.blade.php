72f00b1445f71b4bf9755829d71abd047ec582bd
NickShelud <N.She.1997@mail.ru>
2023-07-24 11:34:42 +0300

    fix safety bugs

--- resources/views/mainWelcome.blade.php
+++ resources/views/mainWelcome.blade.php
@@ -9,10 +9,14 @@
     @extends('layouts.nav')
 
     @section('content')
-        <h1>{{ __('trans.hello') }}</h1>
-        <p>{{ __('trans.descriptionProject') }}</p>
-        <div>
-            <a href="https://github.com/NickShelud" target="_blank">{{ __('trans.clickMe') }}</a>
+    <div class="mr-auto place-self-center lg:col-span-7">
+        
+    </div>
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">{{ __('trans.hello') }}</h1>
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{{ __('trans.descriptionProject') }}</p>
+        <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <a href="https://github.com/NickShelud" target="_blank" 
+            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">{{ __('trans.clickMe') }}</a>
         </div>
     @endsection
 </body>
